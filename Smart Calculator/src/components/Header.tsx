1d3ff270697fe4288e6e9542ba79fe798824434b
ee17btech11034 <asiwalraja@gmail.com>
2026-06-04 11:53:32 +0530

    proj 1: github link add

--- Smart Calculator/src/components/Header.tsx
+++ Smart Calculator/src/components/Header.tsx
@@ -10,6 +10,11 @@ export const Header: React.FC<SectionHeaderProps> = ({ title }) => {
       <h2 className="text-xl sm:text-3xl md:text-4xl font-bold tracking-wide text-gray-900 dark:text-slate-100 transitioning-text">
         {title}
       </h2>
+      <p>
+        <a className='cursor-pointer text-red-500' href='https://github.com/ee17btech11034/Small-Web-Projects/tree/main/Smart%20Calculator' rel="noreferrer" target="_blank">
+          GitHub Repo
+        </a>
+      </p>
     </div>
   );
 };
